203a7676e1bf71f83b97cfcf59ae343b3d26b2f8
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-09-27 15:28:40 -0300

    add rename script

--- I/database/rename.py
+++ I/database/rename.py
@@ -1,15 +1,22 @@
 import os
 
+DIR = "."
+
 REMOVE_WORD = "pdb"
 REMOVE_EXT = ".ent"
 
+NEW_EXT = ".pdb"
+
 def change_extension():
-    for filename in os.listdir("."):
-      if filename.suffix == REMOVE_EXT:
-          os.rename(filename, filename.with_suffix('.pdb').name)
+    for filename in os.listdir(DIR):
+      if filename.endswith(REMOVE_EXT):
+        new_file = filename[:len(REMOVE_EXT)]
+        os.rename(filename, new_file + NEW_EXT)
   
 def remove_start():
-  for filename in os.listdir("."):
+  for filename in os.listdir(DIR):
     if filename.startswith(REMOVE_WORD):
       os.rename(filename, filename[len(REMOVE_WORD):])
-    
+
+remove_start()
+change_extension()
