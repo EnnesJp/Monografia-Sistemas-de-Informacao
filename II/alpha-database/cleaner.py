261059d82e8c5db938da4b1c592f45272654b670
João Ennes <joaoennes@Joaos-MacBook-Air.local>
2024-06-19 22:44:14 -0300

    feat: remove unecessary files

--- II/alpha-database/cleaner.py
+++ II/alpha-database/cleaner.py
@@ -1,19 +1,22 @@
 import os
 
-DIR = "./database"
+DIR = "./"
 FIND_PREFIX = 'ATOM'
 
 def clean_files():
   try:
-    for filename in os.listdir(DIR):
-      with open(f'{DIR}/{filename}', 'r') as fr:
-        lines = fr.readlines()
+    for name in os.listdir(DIR):
+      if name != '.DS_Store' and name != 'cleaner.py':
+        for filename in os.listdir(DIR):
+          with open(f'{DIR}/{filename}', 'r') as fr:
+            lines = fr.readlines()
 
-        with open(f'{DIR}/{filename}', 'w') as fw:
-          for line in lines:
+            with open(f'{DIR}/{filename}', 'w') as fw:
+              for line in lines:
 
-            if line.startswith(FIND_PREFIX):
-                fw.write(line)
+                if line.startswith(FIND_PREFIX):
+                    fw.write(line)
+                    
         print(filename + " clean!")
   except:
     print("Oops! someting error")
