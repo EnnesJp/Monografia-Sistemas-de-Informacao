bd3bb5a78f6a04cb8bf779fe7a094436788dcdb7
João Ennes <joaoennes@Joaos-MacBook-Air.local>
2024-06-19 23:03:00 -0300

    feat: clear all pdb data

--- II/alpha-database/cleaner.py
+++ II/alpha-database/cleaner.py
@@ -6,17 +6,18 @@ FIND_PREFIX = 'ATOM'
 def clean_files():
   try:
     for name in os.listdir(DIR):
+      print(name)
       if name != '.DS_Store' and name != 'cleaner.py':
-        for filename in os.listdir(DIR):
-          with open(f'{DIR}/{filename}', 'r') as fr:
+        for filename in os.listdir(os.path.join(DIR, name)):
+          with open(os.path.join(DIR, name, filename), 'r') as fr:
             lines = fr.readlines()
 
-            with open(f'{DIR}/{filename}', 'w') as fw:
+            with open(os.path.join(DIR, name, filename), 'w') as fw:
               for line in lines:
 
                 if line.startswith(FIND_PREFIX):
                     fw.write(line)
-                    
+
         print(filename + " clean!")
   except:
     print("Oops! someting error")
